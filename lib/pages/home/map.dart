aef519542dc8682a23591f0293743b1fdd7cb43b
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2025-07-21 22:34:23 +0100

    Add app bar

--- lib/pages/home/map.dart
+++ lib/pages/home/map.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:keep_nigeria_clean/widgets/button_group.dart';
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final filters = ['Nearest bins', 'Emptiest bins', 'Low odor bins'];
+
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(),
-      body: Container(
-        color: Theme.of(context).colorScheme.surface,
-        child: Center(
-          child: Text(
-            'Map View',
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 100.0,
+              decoration: BoxDecoration(
+                color: theme.colorScheme.surface,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 1,
+                    offset: Offset(0, 4.0),
+                    color: theme.colorScheme.surfaceDim,
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20.0, 0, 16.0),
+                    child: KNCButtonGroup(
+                      values: filters,
+                      onSelectionChange: (newValues) {},
+                      enableMultiSelection: false,
+                    ),
+                  ),
+                  Row(),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/button_group.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:keep_nigeria_clean/theme/styles.dart';
+
+class KNCButtonGroup extends StatefulWidget {
+  const KNCButtonGroup({
+    required this.values,
+    required this.onSelectionChange,
+    this.scrollable = true,
+    this.enableMultiSelection = true,
+    this.initialSelection,
+    super.key,
+  });
+
+  final List<String> values;
+  final Function(Set<String>) onSelectionChange;
+  final bool scrollable;
+  final bool enableMultiSelection;
+  final Set<String>? initialSelection;
+
+  @override
+  State<KNCButtonGroup> createState() => _KNCButtonGroupState();
+}
+
+class _KNCButtonGroupState extends State<KNCButtonGroup> {
+  late Set<String> _selected;
+
+  @override
+  void initState() {
+    super.initState();
+    _selected = widget.initialSelection ?? {};
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    if (widget.scrollable) {
+      return SizedBox(
+        height: 40.0,
+        child: ListView.separated(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          itemCount: widget.values.length,
+          itemBuilder: (context, i) {
+            final String curr = widget.values[i];
+            if (_selected.contains(curr)) {
+              return TextButton(
+                onPressed: () {
+                  setState(() {
+                    _selected.remove(curr);
+                  });
+
+                  widget.onSelectionChange(_selected);
+                },
+                style: AppButtonStyles.filled,
+                child: Row(
+                  children: [
+                    Text(curr),
+                    SizedBox(width: 12.0),
+                    Icon(Icons.close),
+                  ],
+                ),
+              );
+            }
+            return TextButton(
+              onPressed: () {
+                if (widget.enableMultiSelection ||
+                    (!widget.enableMultiSelection && _selected.isEmpty)) {
+                  setState(() {
+                    _selected.add(curr);
+                  });
+                  widget.onSelectionChange(_selected);
+                }
+              },
+              child: Text(curr),
+            );
+          },
+          separatorBuilder: (context, i) => SizedBox(width: 24.0),
+          scrollDirection: Axis.horizontal,
+        ),
+      );
+    } else {
+      return Wrap(
+        spacing: 16.0,
+        children: List.generate(widget.values.length, (i) {
+          final String curr = widget.values[i];
+          if (_selected.contains(curr)) {
+            return TextButton(
+              onPressed: () {
+                setState(() {
+                  _selected.remove(curr);
+                });
+
+                widget.onSelectionChange(_selected);
+              },
+              style: theme.textButtonTheme.style,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(curr),
+                  SizedBox(width: 12.0),
+                  Icon(Icons.close),
+                ],
+              ),
+            );
+          }
+          return TextButton(
+            onPressed: () {
+              if (widget.enableMultiSelection) {
+                setState(() {
+                  _selected.add(curr);
+                });
+              } else {
+                setState(() {
+                  _selected.clear();
+                  _selected.add(curr);
+                });
+              }
+              widget.onSelectionChange(_selected);
+            },
+            child: Text(curr),
+          );
+        }),
+      );
+    }
+  }
+}
